3cba057573d3c6489c268e1cf8ffeec08484cbf0
KAJSE <162324488+KAJSE@users.noreply.github.com>
2025-10-15 17:04:52 +0800

    set server cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { pool } = require('./event_db');
 const path = require('path');
+const cors = require('cors');
 const categoryController = require('./controller/categoryController');
 const eventController = require('./controller/eventController');
 const organisationController = require('./controller/organisationController');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'web')));
 
