a55c63ccaa59f14327f1562f428801a55469c042
SDHBDJK <162325246+SDHBDJK@users.noreply.github.com>
2025-10-16 20:21:23 +0800

    create admin components

--- web-angular/src/app/app.routes.ts
+++ web-angular/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Home } from './home/home';
 import { Search } from './search/search';
 import { Detail } from './detail/detail';
 import { AddRegistration } from './add-registration/add-registration';
+import { Admin } from './admin/admin';
+import { Categories } from './admin/categories/categories';
+import { Organisations } from './admin/organisations/organisations';
+import { Events } from './admin/events/events';
 
 export const routes: Routes = [
   {
@@ -20,5 +24,15 @@ export const routes: Routes = [
   {
     path: 'register/:id',
     component: AddRegistration
-  }
+  },
+  {
+    path: 'admin',
+    component: Admin,
+    children: [
+      { path: '', redirectTo: 'categories', pathMatch: 'full' },
+      { path: 'categories', component: Categories },
+      { path: 'organisations', component: Organisations },
+      { path: 'events', component: Events },
+    ]
+  },
 ];
